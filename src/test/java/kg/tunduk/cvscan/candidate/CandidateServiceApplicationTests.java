000a270643fbcd681f3bf1293d973f8edbd87c46
ErlanDjamankulov <erlan.djamankulov@gmail.com>
2026-06-23 15:00:28 +0600

    инициализация проекта и начало работы над проектом

---
+++ src/test/java/kg/tunduk/cvscan/candidate/CandidateServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package kg.tunduk.cvscan.candidate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CandidateServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
